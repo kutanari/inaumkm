c75f4ee982c20f457655063de9583966d9e780fa
Fery Tarigan <kutanaridesain@gmail.com>
2023-08-20 15:10:05 +0700

    add base model and migration

---
+++ app/Http/Controllers/NumberOfEmployeeController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\NumberOfEmployee;
+use Illuminate\Http\Request;
+
+class NumberOfEmployeeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(NumberOfEmployee $numberOfEmployee)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(NumberOfEmployee $numberOfEmployee)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, NumberOfEmployee $numberOfEmployee)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(NumberOfEmployee $numberOfEmployee)
+    {
+        //
+    }
+}
