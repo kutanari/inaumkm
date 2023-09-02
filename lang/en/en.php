ee3a20a6c391c3bbbdc6282a37244504ba088b63
Fery Tarigan <kutanaridesain@gmail.com>
2023-09-02 07:08:42 +0700

    add translation for id

---
+++ lang/en/en.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'The :attribute must be at least :length characters and contain at least one uppercase character.' => 'The :attribute palingtidak least :length characters and contain at least one uppercase character.',
+    'Name' => 'Asuuu'
+];
