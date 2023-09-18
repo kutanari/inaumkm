3fcc29706a180cba67ae7d878695858976f4080f
Fery Tarigan <kutanaridesain@gmail.com>
2023-09-18 22:40:01 +0700

    modify dashboard with completion progress

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         
-        <title>{{ config('app.name', 'Inaproduct x RumahUMKM') }}</title>
+        <title>{{ config('app.name', 'Inaproduct x RumahBUMN') }}</title>
         
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
@@ -25,16 +25,17 @@
         </script>
         
         @livewireStyles
+        @stack('styles')
     </head>
-    <body x-data class="font-sans antialiased" :class="$store.overflow.hidden && 'overflow-hidden'" :class="$store.darkMode.on && 'dark'">
+    <body x-data class="font-sans antialiased" :class="$store.overflow.hidden && 'overflow-hidden'">
         <x-banner />
-
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+        
+        <div class="min-h-screen bg-gray-100">
             @livewire('navigation-menu')
         
             <!-- Page Heading -->
             @if (isset($header))
-                <header class="bg-white dark:bg-gray-800 shadow">
+                <header class="bg-white  shadow">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
